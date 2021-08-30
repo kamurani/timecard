08d3ee59042a4986dddccdff258215aaa00b0352
Cam <cameronmcmenamie@gmail.com>
2021-08-31 02:04:49 +1000

    Add setup.sh script.  Use absolute paths when referring to log / metadata files in timecard

--- setup.sh
+++ setup.sh
@@ -1,9 +1,32 @@
 #! /usr/bin/env bash
 
-filename="timecard.py"
-program_name="timecard"
-dir="/usr/bin/$program_name"
 
-chmod +x $filename
+# Assumes ~/bin is in $PATH 
 
-cp $filename $dir 
+# Assumes this script is run in same directory as $filename
+
+# Absolute path of this script
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+
+# Name of directory where script is
+DIR=`dirname $ABSOLUTE_PATH`
+
+TARGET_DIR="$HOME/bin"
+
+#echo `dirname $ABSOLUTE_PATH`
+
+
+FILENAME="timecard.py"
+PROGRAM_NAME="timecard"
+
+SCRIPT_PATH="$DIR/$FILENAME"
+
+chmod +x $SCRIPT_PATH
+
+# Put symlink in $dir
+
+# TODO avoid overwriting if $PROGRAM_NAME already exists in directory
+
+cd $TARGET_DIR
+cp -s $SCRIPT_PATH $PROGRAM_NAME
+
